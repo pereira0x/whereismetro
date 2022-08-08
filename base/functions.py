ccd4fbebec4d316e9334c93793a0bf26a338ef0a
pearsettings44 <jose.a.pereira03@gmail.com>
2022-08-08 20:01:16 +0100

    speedrunnnn

--- base/functions.py
+++ base/functions.py
@@ -1,17 +1,19 @@
-import datetime
+from time import strftime
+from time import gmtime
 
-def processMetro(metro_info, destino):
+
+def processMetro(metro_info, destino, estacoes):
     count = 0
     metros = []
     auxDic = {}
     for metro in metro_info:
         if metro['stop_id'] == 'OD' or metro['stop_id'] == 'RA':
             if metro['stop_id'] == 'OD':
-                auxDic["tempoChegada1"] = str(datetime.timedelta(
-                    seconds=int(metro["tempoChegada1"])))
+                auxDic["tempoChegada1"] = strftime(
+                    "%M:%S", gmtime(int(metro["tempoChegada1"])))
             else:
-                auxDic["tempoChegada2"] = str(datetime.timedelta(
-                    seconds=int(metro["tempoChegada1"])))
+                auxDic["tempoChegada2"] = strftime(
+                    "%M:%S", gmtime(int(metro["tempoChegada1"])))
 
             auxDic["stop_id"] = metro["stop_id"]
             metros.append(auxDic)
@@ -20,20 +22,24 @@ def processMetro(metro_info, destino):
         # For some reason this value is duplicated in the API
         if metro["cais"] == 'CS27O':
             continue
-        # For some reason this value is duplicated in the API
         auxDic["stop_id"] = metro["stop_id"]
         if metro["destino"] == destino:
-            auxDic["tempoChegada2"] = str(datetime.timedelta(
-                seconds=int(metro["tempoChegada1"])))
+            auxDic["tempoChegada2"] = strftime(
+                "%M:%S", gmtime(int(metro["tempoChegada1"])))
             count += 1
         else:
-            auxDic["tempoChegada1"] = str(datetime.timedelta(
-                seconds=int(metro["tempoChegada1"])))
+            auxDic["tempoChegada1"] = strftime(
+                "%M:%S", gmtime(int(metro["tempoChegada1"])))
             count += 1
         if count == 2:
             metros.append(auxDic)
             count = 0
             auxDic = {}
+    
+    i = 0
+    for metro in metros:
+        metro["nome"] = estacoes[i]
+        i += 1
     return metros
 
 
--- base/views.py
+++ base/views.py
@@ -17,12 +17,17 @@ def vermelha(request):
     # Red line order
     my_own_order = ['SS', 'SA', 'AM', 'OL', 'BV',
                     'CH', 'OS', 'CR', 'OR', 'MO', 'EN', 'AP']
+    # Red line stations
+    stations = ['São Sebastião', 'Saldanha', 'Alameda', 'Olaias', 'Bela Vista', 'Chelas',
+                'Olivais', 'Cabo Ruivo', 'Oriente', 'Moscavide', 'Encarnação', 'Aeroporto']
     # Sort the red line data by order
     metro_info = sortMetro(response, my_own_order)
     # Process the red line data
-    metros = processMetro(metro_info, '38')
+    metros = processMetro(metro_info, '38', stations)
 
-    return render(request, 'base/table.html', {'metros': metros})
+    return render(request, 'base/table.html', {'metros': metros,
+                                               'sentido2': "São Sebastião -> -> -> Aeroporto ",
+                                               'sentido1': "Aeroporto -> -> -> São Sebastião"})
 
 
 def verde(request):
@@ -34,13 +39,20 @@ def verde(request):
     # Green line order
     my_own_order = ['TE', 'CG', 'AL', 'RM', 'AE',
                     'AM', 'AR', 'AN', 'IN', 'MM', 'RO', 'BC', 'CS']
+    # Green line stations
+    stations = ['Telheiras',
+                'Campo Grande', 'Alvalade', 'Roma', 'Areeiro', 'Alameda',
+                'Arroios', 'Anjos', 'Intendente', 'Martim Moniz', 'Rossio',
+                'Baixa-Chiado', 'Cais do Sodré']
 
     # Sort the green line data by order
     metro_info = sortMetro(response, my_own_order)
 
-    metros = processMetro(metro_info, '50')
+    metros = processMetro(metro_info, '50', stations)
 
-    return render(request, 'base/table.html', {'metros': metros})
+    return render(request, 'base/table.html', {'metros': metros,
+                                               'sentido2': "Cais do Sodré -> -> -> Telheiras ",
+                                               'sentido1': "Telheiras -> -> -> Cais do Sodré"})
 
 
 def azul(request):
@@ -52,13 +64,21 @@ def azul(request):
     # Blue line order
     my_own_order = ['RB', 'AS', 'AF', 'PO', 'CA', 'CM', 'AH', 'LA', 'JZ', 'PE',
                     'SS', 'PA', 'MP', 'AV',  'RE', 'BC', 'TP', 'SP']
+    # Blue line stations
+    stations = ["Reboleira", "Amadora Este", "Alfornelos", "Pontinha",
+                "Carnide", "Colégio Militar / Luz", "Alto dos Moinhos", "Laranjeiras",
+                "Jardim Zoológico", "Praça de Espanha", "São Sebastião", "Parque",
+                "Marquês do Pombal", "Avenida", "Restauradores", "Baixa-Chiado",
+                "Terreiro do Paço", "Santa Apolónia"]
 
     # Sort the blue line data by order
     metro_info = sortMetro(response, my_own_order)
     # Process the blue line data
-    metros = processMetro(metro_info, '33')
+    metros = processMetro(metro_info, '33', stations)
 
-    return render(request, 'base/table.html', {'metros': metros})
+    return render(request, 'base/table.html', {'metros': metros,
+                                               'sentido1': "Santa Apolónia -> -> -> Reboleira ",
+                                               'sentido2': "Reboleira -> -> -> Santa Apolónia"})
 
 
 def amarela(request):
@@ -72,12 +92,17 @@ def amarela(request):
     # Yellow line order
     my_own_order = ['OD', 'SR', 'AX', 'LU',
                     'QC', 'CG', 'CU', 'EC', 'CP', 'SA', 'PI', 'MP', 'RA']
-    print("\n\n\n\n\n")
+    # Yellow line stations
+    stations = ["Odivelas", "Senhor Roubado", "Ameixoeira", "Lumiar",
+                "Quinta das Conchas", "Campo Grande", "Cidade Universitária",
+                "Entre Campos", "Campo Pequeno", "Saldanha", "Picoas",
+                "Marquês de Pombal", "Rato"]
+
     # Sort the yellow line data by order
     metro_info = sortMetro(response, my_own_order)
-    for y in metro_info:
-        print(y)
     # Process the yellow line data
-    metros = processMetro(metro_info, '43')
+    metros = processMetro(metro_info, '43', stations)
 
-    return render(request, 'base/table.html', {'metros': metros})
+    return render(request, 'base/table.html', {'metros': metros,
+                                               'sentido1': "Rato -> -> -> Odivelas ",
+                                               'sentido2': "Odivelas -> -> -> Rato"})
